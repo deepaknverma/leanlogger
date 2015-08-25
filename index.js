af048becc8169196349a499a36e24511890c4986
Deepak Verma <deepak.verma@hometrack.com>
2015-08-26 09:17:28 +1000

    added debug mode for raw log dumping

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-module.exports = function(request, response, next){
-	var start = +new Date();
-	var stream = process.stdout;
-	var url = request.url;
-	var method = request.method;
 
-	response.on('finish', function(){
-		var duration = +new Date() - start;
-		var message = method + ' to ' + url + ' \ntook ' + duration + ' ms \n\n';
+function leanLogger(options){
+	if(!options)
+		options = {};
 
+	this.debug = true;
+
+	return this;
+}
+
+leanLogger.prototype.log = function log(req, res, next){
+	var self 	= this;
+	var start 	= +new Date();
+	var stream 	= process.stdout;
+
+	// Constructing request params
+	var reqParams = {
+		'url' 			: req.url,
+		'method' 		: req.method,
+		'app'			: req.app,
+		'baseUrl'		: req.baseUrl,
+		'body'			: req.body,
+		'cookies'		: req.cookies,
+		'hostname'		: req.hostname,
+		'ip'			: req.ip,
+		'ips'			: req.ips,
+		'originalUrl'	: req.originalUrl,
+		'params'	 	: req.params,
+		'path'		 	: req.path,
+		'protocol'	 	: req.protocol,
+		'query'			: req.query,
+		'route'			: req.route,
+		'secure'		: req.secure,
+		'signedCookies'	: req.signedCookies,
+		'stale'			: req.stale,
+		'subdomains'	: req.subdomains,
+		'xhr'			: req.xhr
+	};
+
+	res.on('finish', function(){
+		var duration 	= +new Date() - start;
+		var message 	= reqParams.method + ' to ' + reqParams.url + ' \ntook ' + duration + ' ms \n\n';
+
+		if(self.debug)
+			stream.wrtie(reqParams);
+		
 		// prints the log message
 		stream.write(message);
 	});
 
-
 	next();
 }
+
+module.exports = leanLogger;
